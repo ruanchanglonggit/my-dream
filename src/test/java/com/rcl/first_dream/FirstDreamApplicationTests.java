9d087d791553548f1ffe178d6b1d32d63a64aa60
code2 <411377854@qq.com>
2020-01-06 23:25:02 +0800

    新建boot项目

---
+++ src/test/java/com/rcl/first_dream/FirstDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcl.first_dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
